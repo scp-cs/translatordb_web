aec743b778acad9b00ff8c1d7656b1fa0911a9a1
x10102 <x10102.ot@gmail.com>
2023-05-28 20:25:25 +0200

    Add custom error pages

--- App.py
+++ App.py
@@ -1,40 +1,63 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for, abort, flash
 from collections import namedtuple
 import json
 
 import sqlite3
 
+from flask_login import LoginManager, login_required
+from db import Database
+from forms import LoginForm, PasswordChangeForm
+
+dbs = Database(drop=True)
+
 app = Flask(__name__)
+login_manager = LoginManager(app)
 
-db = {}
+login_manager.session_protection = "strong"
 
-User = namedtuple('User', 'nickname discord wikidot articles points role')
+@app.errorhandler(404)
+def e404(e):
+    return render_template('error.j2', errno=404, errtext="Not Found", errquote="Není žádná Antimemetická divize.", errlink="http://scp-cs.wikidot.com/your-last-first-day")
 
-def get_users():
-    _users = []
-    for u in db['users'].items():
-        _users.append(User(u[0], u[1]['discord_id'], u[1]['wikidot'], len(u[1]['articles'].keys()), u[1]['total_points'], u[1]['role_level']))
-    return sorted(_users, key=lambda u: u.points, reverse=True)
+@app.errorhandler(403)
+def e403(e):
+    return render_template('error.j2', errno=403, errtext="Unauthorized", errquote="Okamžitě ukončete své spojení a zůstaňte na místě. Najdeme vás.", errlink="http://scp-cs.wikidot.com/scp-6630")
 
-def db_save():
-    try:
-        with open('translations.json', 'w') as tfile:
-            json.dump(db, tfile)
-    except IOError as e:
-        raise IOError(f"Write error: {str(e)}")
+@app.errorhandler(500)
+def e500(e):
+    return render_template('error.j2', errno=500, errtext="Internal Server Error", errquote="VJEM: CHYBA. HYPERREALITA.", errlink="http://scp-cs.wikidot.com/scp-5500")
+
+@login_manager.user_loader
+def user_loader(id: str):
+    return dbs.get_user(int(id))
 
 @app.route('/')
 def index():
-    return render_template('users.j2', users=get_users())
+    return render_template('users.j2')
+
+@app.route('/abort/<int:e>')
+def error(e:int):
+    abort(e)
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         return "OK"
-    return render_template('login.j2')
+    return render_template('login.j2', form=LoginForm())
+
+@app.route('/pw_change', methods=["GET", "POST"])
+def pw_change():
+    if request.method == "POST":
+        form = PasswordChangeForm()
+        if not form.validate_on_submit():
+            flash("Něco se posralo, zavolejte cheemsovi.", category="error")
+            return redirect(url_for('pw_change'))
+        else:
+            dbs.update_password()
+
+    return render_template('pw_change.j2')
 
 if __name__ == '__main__':
     app.config.from_file('config.json', json.load)
-    with open('translations_new.json', 'r+') as tfile:
-        db = json.load(tfile)
-    app.run(debug=True)
+    dbs.migratejson(pw_for_all=True)
+    app.run()
