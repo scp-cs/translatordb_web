00ea98564263dc8a7e80f47c9732351932b65be1
x10102 <x10102.ot@gmail.com>
2023-09-20 20:13:09 +0200

    Make the sidebar button not cover the username

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["templates/**/*.j2"],
-  safelist: ['animate-fadeout', 'animate-fadein', 'border-green-500', 'border-purple-600', 'border-yellow-400', 'border-red-600', 'border-rose-500'],
+  safelist: ['animate-fadeout', 'animate-fadein', 'border-green-500', 'border-purple-600', 'border-yellow-400', 'border-red-600', 'border-rose-500', 'opacity-0'],
   theme: {
     extend: {
       animation: {
